d60cb06460f6173a7eeda0cac9e35bd28b00dd11
prenl <79540438+prenl@users.noreply.github.com>
2025-08-24 00:31:52 +0500

    feat: schedule api

--- schedule-worker/src/index.ts
+++ schedule-worker/src/index.ts
@@ -1,9 +1,43 @@
 import { Hono } from "hono";
 
+type ScheduleData = {
+    [key: string]: {
+        id: string;
+        start: string;
+        end: string;
+        courseName: string;
+        location: string;
+        isOnline: boolean;
+        teacher: string;
+        type: "lecture" | "practice";
+    }[];
+};
+
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
-app.get("/message", (c) => {
-  return c.text("Hello Hono!");
+app.get("/groups", async (c) => {
+    const schedule = await c.env.SCHEDULE_BUCKET.get("main.json");
+
+    if (!schedule) {
+        return c.json({ error: "Schedule not found" }, 404);
+    }
+
+    const scheduleData: ScheduleData = await schedule.json();
+
+    return c.json(Object.keys(scheduleData));
+});
+
+app.get("/schedule/:group", async (c) => {
+    const group = c.req.param("group");
+    const schedule = await c.env.SCHEDULE_BUCKET.get("main.json");
+
+    if (!schedule) {
+        return c.json({ error: "Schedule not found" }, 404);
+    }
+
+    const scheduleData: ScheduleData = await schedule.json();
+
+    return c.json(scheduleData[group] ?? []);
 });
 
 export default app;
