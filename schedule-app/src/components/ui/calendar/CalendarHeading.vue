fbef6eb0b8f41e132189d54ab24a92a44b67b428
Alex Sehl <widesehl@gmail.com>
2025-09-05 14:22:39 +0500

    fix: ignore errors

--- schedule-app/src/components/ui/calendar/CalendarHeading.vue
+++ schedule-app/src/components/ui/calendar/CalendarHeading.vue
@@ -8,6 +8,7 @@ import { cn } from "@/lib/utils";
 const props = defineProps<CalendarHeadingProps & { class?: HTMLAttributes["class"] }>();
 
 defineSlots<{
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   default: (props: { headingValue: string }) => any;
 }>();
 
