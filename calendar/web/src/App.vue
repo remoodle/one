112334b3e7ce9b34c3fe2636d829b275be75a942
Alex Sehl <widesehl@gmail.com>
2025-09-15 20:14:45 +0500

    refactor: use watchEffect for themeswitch

--- calendar/web/src/App.vue
+++ calendar/web/src/App.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
-import { watch } from "vue";
+import { watchEffect } from "vue";
 import { RouterView } from "vue-router";
 import { useAppStore } from "@/stores/app";
 
 const appStore = useAppStore();
 
-watch(
-  () => appStore.theme,
-  (value) => {
-    document.documentElement.setAttribute("data-theme", value);
-  },
-  { immediate: true },
-);
+watchEffect(() => {
+  document.documentElement.setAttribute("data-theme", appStore.theme);
+});
 </script>
 
 <template>
